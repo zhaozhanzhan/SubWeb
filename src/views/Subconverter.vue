07a0e18d2166733a2cd3d21db078ad44fd8e835c
zhaozhanzhan <799316652@qq.com>
2022-07-25 18:00:16 +0800

    修改文字样式

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -4,10 +4,10 @@
       <el-col>
         <el-card>
           <div slot="header">
-            Subscription Converter
-            <svg-icon icon-class="github" style="margin-left: 20px" @click="goToProject" />
+            订阅转换
+            <svg-icon icon-class="github" style="margin-left: 10px" @click="goToProject" />
 
-            <div style="display: inline-block; position:absolute; right: 20px">{{ backendVersion }}</div>
+            <!-- <div style="display: inline-block; position:absolute; right: 20px">{{ backendVersion }}</div> -->
           </div>
           <el-container>
             <el-form :model="form" label-width="80px" label-position="left" style="width: 100%">
